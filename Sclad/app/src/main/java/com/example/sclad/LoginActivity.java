4133a5800d798d3386d6c2fc2147254d99714342
Tomas Kovalcik <tom.kovalchik@gmail.com>
2020-04-14 22:52:05 +0200

    first commit-dokopy nic

---
+++ Sclad/app/src/main/java/com/example/sclad/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.sclad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        configureloginbtn();
+    }
+    private void configureloginbtn() {
+        Button loginBtn = (Button) findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, DashBoardActivity.class));
+            }
+        });
+    }
+}   

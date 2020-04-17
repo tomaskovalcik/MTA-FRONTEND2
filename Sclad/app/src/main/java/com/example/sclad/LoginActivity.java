348343e6679b7fb9b5b442fa7aed3d39f6dfd2d4
lmisaga <l.misaga@gmail.com>
2020-04-17 22:55:20 +0200

    API call while logging in, auth setup for cleartext messaging with server

--- Sclad/app/src/main/java/com/example/sclad/LoginActivity.java
+++ Sclad/app/src/main/java/com/example/sclad/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.example.sclad;
 
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.example.sclad.Utils.BasicAuthInterceptor;
+import okhttp3.*;
+
+import java.io.IOException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,15 +19,43 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
-        configureloginbtn();
+        configureLoginBtn();
     }
-    private void configureloginbtn() {
-        Button loginBtn = (Button) findViewById(R.id.login_btn);
+
+    private void configureLoginBtn() {
+        Button loginBtn = findViewById(R.id.login_btn);
+        EditText usernameInput = findViewById(R.id.username_input);
+        EditText passwordInput = findViewById(R.id.password_input);
+        final String username = usernameInput.getText().toString();
+        final String password = passwordInput.getText().toString();
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, DashBoardActivity.class));
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .addInterceptor(new BasicAuthInterceptor(username, password))
+                        .build();
+                final String url = "http://10.0.2.2:8080/api/user/getCurrentlyLoggedUser";
+                Request request = new Request.Builder().url(url).build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.err.println("call with url " + url + " failed.");
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.body() != null) {
+                            System.out.println("response: " + response.body().string());
+                            //TODO check if response == 200, if not => display error message
+                            startActivity(new Intent(LoginActivity.this, DashBoardActivity.class));
+                        } else {
+                            System.err.println("Null response body");
+                        }
+                    }
+                });
+
             }
         });
     }
-}   
+}

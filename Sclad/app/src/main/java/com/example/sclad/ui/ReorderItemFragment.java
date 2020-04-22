47e3b4f702032d1de700d3eca0915f31a700eba6
lmisaga <l.misaga@gmail.com>
2020-04-22 21:54:22 +0200

    item quantity when product is selected in reorder items, return to dashboard after submitting

--- Sclad/app/src/main/java/com/example/sclad/ui/ReorderItemFragment.java
+++ Sclad/app/src/main/java/com/example/sclad/ui/ReorderItemFragment.java
@@ -1,11 +1,11 @@
 package com.example.sclad.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import android.widget.*;
 import androidx.fragment.app.Fragment;
+import com.example.sclad.DashBoardActivity;
 import com.example.sclad.R;
 import com.example.sclad.Utils.BasicAuthInterceptor;
 import com.example.sclad.Utils.JsonHelper;
@@ -30,7 +30,10 @@ public class ReorderItemFragment extends Fragment {
     private SeekBar quantitySeekBar;
     private Switch sendNotificationSwitch;
     private TextView quantityTextView;
+    private TextView selectedProductQuantity;
     private Button submitBtn;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public ReorderItemFragment() {
         // Required empty public constructor
@@ -58,7 +61,44 @@ public class ReorderItemFragment extends Fragment {
         this.submitBtn = view.findViewById(R.id.submitBtn);
         this.quantityTextView = view.findViewById(R.id.quantityTextView);
         this.quantityTextView.setText(Integer.toString(this.quantitySeekBar.getProgress()));
+        this.selectedProductQuantity = view.findViewById(R.id.selectedProductQuantity);
 
+        productNameInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!productNameInput.getText().toString().isEmpty()) {
+                    String productName = productNameInput.getText().toString();
+                    OkHttpClient client = new OkHttpClient.Builder()
+                            .addInterceptor(new BasicAuthInterceptor(SecurityContextHolder.username,
+                                    SecurityContextHolder.password)).build();
+                    final String url = "http://10.0.2.2:8080/api/device/getDeviceByProductName/" + productName;
+                    Request request = new Request.Builder().url(url).build();
+                    client.newCall(request).enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            // do nothing
+                        }
+
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            Response httpResponse = response;
+                            ResponseBody body = response.body();
+                            if (httpResponse.code() == 200 && body != null) {
+                                Device device = objectMapper.readValue(body.string(), Device.class);
+                                String returnString = "Product quantity: " + device.getQuantity();
+                                if (device.getReordered()) {
+                                    returnString = returnString + " | Is reordered. ";
+                                } else {
+                                    returnString = returnString + " | Is not reordered. ";
+                                }
+                                String finalReturnString = returnString;
+                                getActivity().runOnUiThread(() -> selectedProductQuantity.setText(finalReturnString));
+                            }
+                        }
+                    });
+                }
+            }
+        });
         quantitySeekBar.setOnSeekBarChangeListener(onChangeListener);
         submitBtn.setOnClickListener(onClickListener);
         return view;
@@ -69,6 +109,7 @@ public class ReorderItemFragment extends Fragment {
         String productName = productNameInput.getText().toString();
         Boolean sendNotification = sendNotificationSwitch.isChecked();
         Integer quantity = quantitySeekBar.getProgress();
+        //TODO category check?
         String category = categoryDropdown.getSelectedItem().toString();
         OkHttpClient client = new OkHttpClient.Builder()
                 .addInterceptor(new BasicAuthInterceptor(SecurityContextHolder.username,
@@ -104,6 +145,8 @@ public class ReorderItemFragment extends Fragment {
                             public void run() {
                                 Toast t = Toast.makeText(getContext(), "Created restock order for device.", Toast.LENGTH_SHORT);
                                 t.show();
+                                startActivity(new Intent(getActivity(),
+                                        DashBoardActivity.class).putExtra("USERNAME", SecurityContextHolder.username));
                             }
                         });
                     } else {

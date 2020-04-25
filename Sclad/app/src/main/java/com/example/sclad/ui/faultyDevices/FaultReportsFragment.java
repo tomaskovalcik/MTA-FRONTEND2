f144fd5d001117379f526107c4e6ed54e36f12ed
lmisaga <l.misaga@gmail.com>
2020-04-25 15:16:03 +0200

    Downloading attachments for fault reports and resolving - temp

--- Sclad/app/src/main/java/com/example/sclad/ui/faultyDevices/FaultReportsFragment.java
+++ Sclad/app/src/main/java/com/example/sclad/ui/faultyDevices/FaultReportsFragment.java
@@ -1,19 +1,22 @@
 package com.example.sclad.ui.faultyDevices;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
-
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.ListAdapter;
 import com.example.sclad.R;
 import com.example.sclad.Utils.BasicAuthInterceptor;
 import com.example.sclad.Utils.JsonHelper;
@@ -26,7 +29,6 @@ import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class FaultReportsFragment extends Fragment {
 
@@ -44,13 +46,10 @@ public class FaultReportsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_fault_reports, container, false);
         this.faultReportsList = view.findViewById(R.id.fault_reports_list);
         populateList();
-        this.faultReportsList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                showActionsDialog(getActivity(), "Choose action", faultReports.get(position).getFaultDescription(), faultReports.get(position));
-                return false;
+        this.faultReportsList.setOnItemLongClickListener((parent, view1, position, id) -> {
+            showActionsDialog(getActivity(), "Choose action", faultReports.get(position).getFaultDescription(), faultReports.get(position));
+            return false;
 
-            }
         });
         return view;
     }
@@ -63,34 +62,74 @@ public class FaultReportsFragment extends Fragment {
         }
         builder.setMessage(message);
         if (selectedFaultReport.getAttachmentId() != null) {
+
             builder.setPositiveButton("Download attachment", (dialog, which) -> {
-                Request request = new Request
-                        .Builder()
-                        .get()
-                        .url(UrlHelper.resolveApiEndpoint("/api/uploadedFile/downloadByFileName/" + selectedFaultReport.getAttachment().getFileName()))
-                        .build();
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        ToastDisplayHelper.displayShortToastMessage("Unspecified server error.", getActivity());
-                    }
+//                Request request = new Request
+//                        .Builder()
+//                        .get()
+//                        .url(UrlHelper.resolveApiEndpoint("/api/uploadedFile/downloadByFileName/" + selectedFaultReport.getAttachment().getFileName()))
+//                        .build();
+//                client.newCall(request).enqueue(new Callback() {
+//                    @Override
+//                    public void onFailure(Call call, IOException e) {
+//                        ToastDisplayHelper.displayShortToastMessage("Unspecified server error.", getActivity());
+//                    }
+//
+//                    @Override
+//                    public void onResponse(Call call, Response response) throws IOException {
+//                        System.out.println("succ " + response.code());
+//                    }
+//                });
+                if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                        ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                        ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
 
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException {
-                        System.out.println("succ " + response.code());
+                    // this will request for permission when user has not granted permission for the app
+                    ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.INTERNET}, 1);
+                } else {
+                    if (isDownloadManagerAvailable()) {
+                        DownloadManager downloadManager = (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
+                        Uri uri = Uri.parse(UrlHelper.resolveApiEndpoint("/api/uploadedFile/downloadByFileName/" + selectedFaultReport.getAttachment().getFileName()));
+                        DownloadManager.Request request = new DownloadManager.Request(uri);
+                        request.setVisibleInDownloadsUi(true);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                            request.allowScanningByMediaScanner();
+                            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                        }
+                        request.setMimeType(selectedFaultReport.getAttachment().getFileType());
+                        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, uri.getLastPathSegment());
+                        downloadManager.enqueue(request);
                     }
-                });
-
-                //TODO download attachment
+                }
 
             });
         }
         builder.setNeutralButton("Resolve", (dialog, which) -> {
-            //todo delete call to backend
+            Request request = new Request
+                    .Builder()
+                    .delete()
+                    .url(UrlHelper.resolveApiEndpoint("/api/defectReport/resolve/" + selectedFaultReport.getId()))
+                    .build();
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    ToastDisplayHelper.displayShortToastMessage("Unspecified server error.", getActivity());
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.code() == 200) {
+                        ToastDisplayHelper.displayShortToastMessage("Fault report " + selectedFaultReport.getId() + " successfully resolved.", getActivity());
+                    }
+                }
+            });
         });
         builder.show();
     }
 
+    private static boolean isDownloadManagerAvailable() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+    }
 
     private void populateList() {
         OkHttpClient client = BasicAuthInterceptor.buildClientWithInterceptor();
